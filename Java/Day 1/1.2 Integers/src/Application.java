2820199601c976d1715ce961cfed05a9b97751b2
Ntsoumou <Manuel.Ntsoumou@hotmail.com>
2017-09-14 17:04:48 +0400

    Here are all the files from day 1.
    Today, we have started learning Java.

---
+++ Java/Day 1/1.2 Integers/src/Application.java
@@ -0,0 +1,28 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		int myNumber = 16 ;
+		short myShort = 5;
+		long myLong = 100;
+		
+		double myDouble = 5; 
+		float myFloat = 10;
+		
+		char myChar = 'M';
+		boolean myBoolean = false; 
+		
+		byte myByte = 127; 
+		
+		System.out.println(myByte);
+		System.out.println(myChar);
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myBoolean);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+	}
+
+}
---
+++ Java/Day 1/1.3 Strings/src/Application.java
@@ -0,0 +1,23 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		int myInt = 18;
+		
+		
+		
+		String text = "Hello";
+		String blank = " ";
+		String name = "Manuel";
+		
+		String greetings = text + blank + name;
+		
+		System.out.println(text);
+		
+		System.out.println("Hello"+" "+"Manuel");
+		System.out.println(greetings);
+		
+		System.out.println("My integer is:" + " " + myInt +".");
+		
+	}
+}
